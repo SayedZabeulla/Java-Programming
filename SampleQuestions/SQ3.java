1d18d8040a23f7081d4a65d382a3e628e1ed0cd6
Sayed Zabeulla <sayedzabeulla@gmail.com>
2025-07-17 19:47:54 +0530

    Intial Commit

--- SampleQuestions/SQ3.java
+++ SampleQuestions/SQ3.java
@@ -1,18 +1,24 @@
 import java.util.Scanner;
 public class SQ3 {
     public static void main(String[] args){
+
         Scanner sc =new Scanner(System.in);
         String str0 =sc.nextLine();
         String str1 =sc.nextLine();
-        int c =0;
-        while(str0.length()>0){
-            for(int i=0; i<str1.length(); i++){
-                if (str1 == str0){
-                    c++;
+        int c0 =0;
+        char ch1 [] = str0.toCharArray();
+        char ch2 [] =str1.toCharArray();
+        for(int i=0 ; i<str1.length(); i++){
+            for (int j=0; j<str0.length(); j++){
+                if (ch2[i] == ch1[j]){
+                    c0++;
                 }
             }
-            
         }
-        System.out.println(c);
+        int c1 =0;
+        for (int i=0; i<str1.length(); i++){
+            c1++;
+        }
+        System.out.println(c0/c1);
     }
 }
